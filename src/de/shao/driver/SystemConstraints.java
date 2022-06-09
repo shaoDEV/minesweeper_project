f4523c35ac36427d63c3ab505a80f480e51ea742
Marvin <marvindamnik@gmail.com>
2022-06-09 20:59:56 +0200

    Game Refactored

---
+++ src/de/shao/driver/SystemConstraints.java
@@ -0,0 +1,22 @@
+package de.shao.driver;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum SystemConstraints {
+
+    TEN_TEN(new Point(66,72)),
+    SIXTEEN_SIXTEEN(new Point(66,72)),
+    TWENTY_TWENTY(new Point(66,72));
+
+    private Point point;
+
+    SystemConstraints(Point startPoint) {
+        point = startPoint;
+    }
+
+    public Point point(){
+        return point;
+    }
+}
---
+++ src/de/shao/gameRefactor/EndScene.java
@@ -0,0 +1,57 @@
+package de.shao.gameRefactor;
+
+import de.shao.driver.PictureController;
+import de.shao.driver.SystemConstraints;
+
+import javax.imageio.stream.ImageOutputStreamImpl;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class EndScene {
+
+    PictureController pictureController = null;
+    Point drawPoint = null;
+
+    Rectangle backToMenuArea = null;
+    Rectangle startNewGameArea = null;
+
+    Image backToMenu = null;
+    Image startNewGame = null;
+
+    boolean goBackToMenu = false;
+    boolean startANewGame = false;
+
+
+    EndScene(PictureController pictureController, Point drawPoint){
+        this.pictureController = pictureController;
+        this.drawPoint = drawPoint;
+
+        backToMenu = pictureController.getSystemResources("backToMenu");
+        startNewGame = pictureController.getSystemResources("startNewGame");
+
+        backToMenuArea = new Rectangle(drawPoint.x, drawPoint.y, backToMenu.getWidth(null), backToMenu.getHeight(null));
+        startNewGameArea = new Rectangle(drawPoint.x, drawPoint.y + 10 + startNewGame.getHeight(null), startNewGame.getWidth(null), startNewGame.getHeight(null));
+    }
+
+    public void drawScene(Graphics2D g2d){
+        g2d.drawImage(backToMenu, drawPoint.x, drawPoint.y, null);
+        g2d.drawImage(startNewGame, drawPoint.x, drawPoint.y + 10 + startNewGame.getHeight(null), null);
+    }
+
+    public void sceneInteraction(MouseEvent mouseEvent){
+        if (backToMenuArea.contains(mouseEvent.getPoint())){
+            goBackToMenu = true;
+        }
+        if (startNewGameArea.contains(mouseEvent.getPoint())){
+            startANewGame = true;
+        }
+    }
+
+    public boolean isGoBackToMenu() {
+        return goBackToMenu;
+    }
+
+    public boolean isStartANewGame() {
+        return startANewGame;
+    }
+}
